3e98ed92d4e965b6b25642566afac119a2422182
Rebzzel <rebzzel@gmail.com>
2017-12-04 02:04:49 +0300

    Code comments edited

--- d3d11hook.h
+++ d3d11hook.h
@@ -25,7 +25,7 @@ struct ID3D11Device; // from d3d11.h
 struct ID3D11DeviceContext; // from d3d11.h
 struct IDXGISwapChain; // from d3d11.h
 
-// Use for rendering graphical user interfaces (for example: ImGui)
+// Use for rendering graphical user interfaces (for example: ImGui) or other.
 extern D3D11_HOOK_API void ImplHookDX11_Present(ID3D11Device *device, ID3D11DeviceContext *ctx, IDXGISwapChain *swap_chain);
 
 // Main function in Hook. 
